1053316273f455e050ce8c6e67c1e902981ba998
Wboakye <wboak001@fiu.edu>
2019-05-25 11:38:09 -0400

    added Both option

--- IB.py
+++ IB.py
@@ -4,10 +4,10 @@ import time
 
 UN = str(input("Type Username: "))
 PW = str(input("Type Password: "))
-option = str.lower(str(input("Like or Add? ")))
+option = str.lower(str(input("Like, Follow or Both? ")))
 print("Input 5 hashtags")
 hashtags = [str(input("#")), str(input("#")), str(input("#")), str(input("#")), str(input("#"))]
-print("Initiating...")
+print("Initializing...")
 
 
 class InstagramBot:
@@ -63,7 +63,7 @@ class InstagramBot:
             except Exception as e:
                 time.sleep(2)
 
-    def add_users(self, hashtag):
+    def follow_users(self, hashtag):
         #gather data
         driver = self.driver
         print("Gathering posts for #" + hashtag + ".")
@@ -77,7 +77,7 @@ class InstagramBot:
         hrefs = driver.find_elements_by_tag_name('a')
         pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
         #pic_hrefs = [href for href in pic_hrefs if hashtag in href]
-        print("Number of posts: " + str(len(pic_hrefs)) + ". Adding iniciated.")
+        print("Number of posts: " + str(len(pic_hrefs)) + ". Following iniciated.")
         count = len(pic_hrefs)
 
         for pic_href in pic_hrefs:
@@ -93,9 +93,43 @@ class InstagramBot:
             except Exception as e:
                 time.sleep(2)
 
+    def likeAndFollow(self, hashtag):
+        #gather data
+        driver = self.driver
+        print("Gathering posts for #" + hashtag + ".")
+        driver.get("https://www.instagram.com/explore/tags/"+ hashtag +"/")
+        time.sleep(3)
+        for i in range(1, 9):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+        #filter profiles
+        hrefs = driver.find_elements_by_tag_name('a')
+        pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
+        #pic_hrefs = [href for href in pic_hrefs if hashtag in href] //Not working
+        print("Number of posts: " + str(len(pic_hrefs)) + ". Following iniciated.")
+        count = len(pic_hrefs)
+
+        for pic_href in pic_hrefs:
+            driver.get(pic_href)
+            try:
+                time.sleep(2)
+                #like
+                driver.find_element_by_class_name("dCJp8").click()
+                time.sleep(20)
+                #follow
+                driver.find_element_by_class_name("oW_lN").click()
+                count = count - 1
+                print(str(count) + "/" + str(len(pic_hrefs)) + " remaining.")
+                time.sleep(18)
+            except Exception as e:
+                time.sleep(2)
+
+
 
 
 targetIG = InstagramBot(UN, PW)
+print("Logging in...")
 targetIG.login()
 if option == "like":
     targetIG.like_photos(hashtags[0])
@@ -103,12 +137,18 @@ if option == "like":
     targetIG.like_photos(hashtags[2])
     targetIG.like_photos(hashtags[3])
     targetIG.like_photos(hashtags[4])
-elif option == "add":
-    targetIG.add_users(hashtags[0])
-    targetIG.add_users(hashtags[1])
-    targetIG.add_users(hashtags[2])
-    targetIG.add_users(hashtags[3])
-    targetIG.add_users(hashtags[4])
+elif option == "follow":
+    targetIG.follow_users(hashtags[0])
+    targetIG.follow_users(hashtags[1])
+    targetIG.follow_users(hashtags[2])
+    targetIG.follow_users(hashtags[3])
+    targetIG.follow_users(hashtags[4])
+elif option == "both":
+    targetIG.likeAndFollow(hashtags[0])
+    targetIG.likeAndFollow(hashtags[1])
+    targetIG.likeAndFollow(hashtags[2])
+    targetIG.likeAndFollow(hashtags[3])
+    targetIG.likeAndFollow(hashtags[4])
 else: 
     print("Invalid input.")
 
